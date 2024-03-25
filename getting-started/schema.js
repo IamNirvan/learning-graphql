d910c74ba7db2d5bdfd01f14437a5984f694ba74
Shalin Kulawardane <nirvan.work@gmail.com>
2024-03-25 13:47:53 +0530

    added graphQL schemas

---
+++ getting-started/schema.js
@@ -0,0 +1,26 @@
+export const typeDefs = `#graphql
+    type Game {
+        id: ID!
+        title: String!
+        platform: [String!]!
+    }
+
+    type Review {
+        id: ID!
+        rating: Int!
+        content: String!
+    }
+
+    type Author {
+        id: ID!
+        name: String!
+        verified: Boolean!
+    }
+
+    # Need to have a query type. Defines the entry points to the graph and the values that can be returned...
+    type Query {
+        reviews: [Review]
+        games: [Games]
+        authors: [Author]
+    }
+`
