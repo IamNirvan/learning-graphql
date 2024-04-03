1ed2a45ade614e35261246b0cc4ec1be60c6a7e4
Shalin Kulawardane <nirvan.work@gmail.com>
2024-04-03 23:15:34 +0530

    added graphql server as middleware

--- sample-project-3/src/app.ts
+++ sample-project-3/src/app.ts
@@ -1,7 +1,32 @@
 import resolvers from "./graphQL/resolvers/resolver.js";
 import typeDefs from "./graphQL/schemas/schema.js";
 import { IGraphQLServer } from "./graphQL/server/IGraphQLServer.js";
-import { GraphQLServerVersion1 } from "./graphQL/server/impl/graphQlServerV1.js";
+import { GraphQLServerVersion2 } from "./graphQL/server/impl/graphQLServerV2.js";
+import { expressMiddleware } from '@apollo/server/express4';
 
-const server: IGraphQLServer = new GraphQLServerVersion1(4000, typeDefs, resolvers)
-server.start()
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+// const server: IGraphQLServer = new GraphQLServerVersion1(4000, typeDefs, resolvers)
+// server.start()
+
+
+const app = express();
+const httpServer = http.createServer(app);
+const graphqlServer = new GraphQLServerVersion2(typeDefs, resolvers, httpServer);
+await graphqlServer.start();
+
+// setup the middleware
+app.use(
+    '/graphql',
+    cors<cors.CorsRequest>(),
+    express.json(),
+    expressMiddleware(graphqlServer.getServer(),{  
+        context: graphqlServer.createContext
+    }),
+);
+
+// Start the server
+const port = 4000;
+await new Promise<void>((resolve) => httpServer.listen({ port: port }, resolve));
+console.log(`server started at http://localhost:${port}/graphql`);
---
+++ sample-project-3/src/graphQL/server/impl/graphQLServerV2.ts
@@ -0,0 +1,63 @@
+import { ApolloServer, BaseContext } from '@apollo/server';
+import { startStandaloneServer } from "@apollo/server/standalone"
+import { IGraphQLServer } from "../IGraphQLServer.js";
+import { Context } from "../../context/Context.js";
+import { CustomPlugin } from '../../plugins/CustomPlugin.js';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+
+/**
+ * This implementation uses the middleware approach
+ * @author Shalin Kulawardane
+ */
+export class GraphQLServerVersion2 implements IGraphQLServer {
+    readonly typeDefs: any;
+    readonly resolvers: any;
+    readonly httpServer: any;
+    private readonly server: ApolloServer<Context>;
+    
+    constructor(typeDefs: any, resolvers: any , httpServer: any) {
+        this.typeDefs = typeDefs;
+        this.resolvers = resolvers;
+        this.httpServer = httpServer;
+        this.server = this._createServer();
+    }
+
+    /**
+     * Initializes the Apollo server instance and adds default configurations
+     * @returns ApolloServer instance using custom context
+     */
+    private _createServer(): ApolloServer<Context> {
+        let result =  new ApolloServer<Context>({
+            typeDefs: this.typeDefs,
+            resolvers: this.resolvers,
+            includeStacktraceInErrorResponses: false,
+            plugins: [
+                ApolloServerPluginDrainHttpServer({ httpServer: this.httpServer })
+            ]
+        });
+        return result
+    }
+
+    /**
+     * Creates the context object for the server.
+     * @returns A Promise that resolves to the context object.
+     */
+    async createContext(): Promise<Context> {
+        const context = {
+            dbConnection: "mongodb://localhost:27017",
+        }
+        return context;
+    }
+
+    async start(): Promise<void> {
+        await this.server.start();
+    }
+
+    async stop(): Promise<void> {
+        throw new Error('Method not implemented.');
+    }
+
+    getServer(): ApolloServer<Context> {
+        return this.server;
+    }
+}
